d33aeaf06a35853ce81ddb48c446c4d21b2af396
bodqhrohro <bodqhrohro@gmail.com>
2018-11-01 09:19:45 +0200

    Allow multiple authors for a book

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
+author_book_table = Table('author_book', Base.metadata,
+                          Column('author_id'), Integer, ForeignKey('author.id'),
+                          Column('book_id'), Integer, ForeignKey('book.id'))
 
 class Author(Base):
     __tablename__ = 'author'
@@ -13,13 +17,16 @@ class Author(Base):
     def __str__(self):
         return self.name
 
+
 class Book(Base):
     __tablename__ = 'book'
     id = Column(Integer, primary_key=True)
     isbn = Column(String(13), unique=True, nullable=True)
     title = Column(String, nullable=True)
     annotation = Column(String)
-    authors = Column(Integer, ForeignKey(Author.id))
+    authors = relationship('Author',
+                           secondary=author_book_table,
+                           backref='books')
 
     def __str__(self):
         return self.title
